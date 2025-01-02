7985a2c6d89c06e469c686140b94b8710f4b5b9a
Gustavo Colombo <gusttavocolombo@gmail.com>
2025-01-02 11:09:59 -0300

    feat: setup jwt and cookies

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url().min(1),
   API_BASE_URL: z.string().url().min(1),
   API_REDIRECT_URL: z.string().url().min(1),
+  JWT_SECRET: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,24 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import { jwt } from '@elysiajs/jwt'
+import { cookie } from '@elysiajs/cookie'
+import { env } from '../env'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+      exp: '1d',
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('Bun server started on port 3333!')
