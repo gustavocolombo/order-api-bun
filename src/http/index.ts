f1887f4b86a7faf580c9e9e8012233a1f1d07632
Gustavo Colombo <gusttavocolombo@gmail.com>
2025-01-12 23:48:25 -0300

    feat: adding missing routes to manipulate orders

--- src/http/index.ts
+++ src/http/index.ts
@@ -7,6 +7,9 @@ import { getCurrentProfile } from './routes/get-current-profile'
 import { getManagedRestaurants } from './routes/get-managed-restaurants'
 import { getOrderDetails } from './routes/get-order-details'
 import { approveOrder } from './routes/approve-order'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -17,6 +20,9 @@ const app = new Elysia()
   .use(getManagedRestaurants)
   .use(getOrderDetails)
   .use(approveOrder)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
 
 app.listen(3333, () => {
   console.log('Bun server started on port 3333!')
