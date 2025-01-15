fc478e07e49b466374a094450601b4aa275ebe1c
Gustavo Colombo <gusttavocolombo@gmail.com>
2025-01-14 22:53:16 -0300

    feat: adding ordering by created_at and status

--- src/http/routes/get-orders.ts
+++ src/http/routes/get-orders.ts
@@ -2,7 +2,7 @@ import Elysia, { t } from 'elysia'
 import { auth } from '../auth'
 import { orders, users } from '../../db/schema'
 import { db } from '../../db/connection'
-import { and, count, eq, getTableColumns, ilike } from 'drizzle-orm'
+import { and, count, desc, eq, ilike, sql } from 'drizzle-orm'
 import { createSelectSchema } from 'drizzle-typebox'
 
 export const getOrders = new Elysia().use(auth).get(
@@ -13,10 +13,14 @@ export const getOrders = new Elysia().use(auth).get(
 
     if (!restaurantId) throw new Error('User is not a manager')
 
-    const ordersTableColumns = getTableColumns(orders)
-
     const baseQuery = db
-      .select(ordersTableColumns)
+      .select({
+        oderId: orders.id,
+        customerName: users.name,
+        createdAt: orders.createdAt,
+        status: orders.status,
+        total: orders.totalInCents,
+      })
       .from(orders)
       .innerJoin(users, eq(users.id, orders.customerId))
       .where(
@@ -34,7 +38,19 @@ export const getOrders = new Elysia().use(auth).get(
         .select()
         .from(baseQuery.as('baseQuery'))
         .offset(pageIndex * 10)
-        .limit(10),
+        .limit(10)
+        .orderBy((fields) => {
+          return [
+            sql`CASE ${fields.status}
+              WHEN 'pending' THEN 1
+              WHEN 'processing' THEN 2
+              WHEN 'delivering' THEN 3
+              WHEN 'delivered' THEN 4
+              WHEN 'canceled' THEN 99
+            END`,
+            desc(sql`"baseQuery"."created_at"`),
+          ]
+        }),
     ])
 
     const amountOfOrders = amounOfOrdersQuery[0].count
